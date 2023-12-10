53529168b53b5865b6bf735de006d7e34907fa12
Andrey <apiddubnij21@gmail.com>
2023-12-09 19:52:18 -0500

    Added Project and made Sign in button

--- Loginfirebase/LoginfirebaseApp.swift
+++ Loginfirebase/LoginfirebaseApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct LoginfirebaseApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+        print("Configured Firebase!")
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
 }
