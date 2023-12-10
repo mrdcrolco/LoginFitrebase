ab3c219ed39bb036e6a16c5922604ae80af53fde
Andrey <apiddubnij21@gmail.com>
2023-12-09 19:20:35 -0500

    Initial Commit

---
+++ Loginfirebase/LoginfirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginfirebaseApp.swift
+//  Loginfirebase
+//
+//  Created by Andrii Piddubnyi on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginfirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
